cd24d578d2303b28fcd1bc00651ca3794c6a9ce4
OlivierDupre <dupreolivier@gmail.com>
2013-03-12 13:39:50 +0100

    sauvegarde onglet et recherche full text

---
+++ public_html/js/tabManager.js
@@ -0,0 +1,33 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$('.nav-tabs a').click(function(e) {
+    e.preventDefault();
+    $(this).tab('show');
+    saveActiveTab();
+});
+
+function saveActiveTab() {
+    window.localStorage.setItem("ActiveHref", $('.nav-tabs li.active a').attr("href"));
+}
+
+function getActiveTab() {
+    var href = window.localStorage.getItem("ActiveHref");
+    return $('.nav-tabs a[href="' + href + '"]');
+}
+
+$(document).ready(function() {
+    getActiveTab().tab('show');
+});
+
+
+$('#choix_recherche').keyup(function() {
+    if ($(this).val().length <= 3)
+        return;
+    
+    var content = $('.tab-content .tab-pane.active').html();
+    $('.tab-content .tab-pane.active').html(content.replace(new RegExp($(this).val(), 'g'), "<span class='highlight'>" + $(this).val() + "</span>"));
+});
